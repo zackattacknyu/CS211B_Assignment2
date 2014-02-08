086e4370198511d840cb5ff65604629204a1dedf
zackattacknyu <zackattacknyu@gmail.com>
2014-02-07 16:28:01 -0800

    improved rendering. made it done by pixel order

--- src/init/RandomNoise.java
+++ src/init/RandomNoise.java
@@ -15,19 +15,36 @@ public class RandomNoise {
 		int imageHeight = 700;
 		int imageWidth = 1000;
 		
-		//this is used to generate the image data
+		//this array is used to generate the image data
 		int[][][] imageData = new int[3][imageWidth][imageHeight];
-		for(int channelNo = 0; channelNo < imageData.length; channelNo++){
-			for(int rowNum = 0; rowNum < imageData[channelNo].length; rowNum++){
+		int currentAmp = 0;
+		double height = imageHeight;
+		double width = imageWidth;
+		double rowN = 0;
+		double colN = 0;
+		double scaleFactor = 0;
+		
+		System.out.println("Now Computing Image Data");
+		
+		//loops through all the pixels generating them
+		for(int rowNum = 0; rowNum < imageData[0].length; rowNum++){
+			
+			for(int colNum = 0; colNum < imageData[0][rowNum].length; colNum++){
 				
-				for(int colNum = 0; colNum < imageData[channelNo][rowNum].length; colNum++){
-					//imageData[channelNo][rowNum][colNum] = (int)(256*Math.random());
-					imageData[channelNo][rowNum][colNum] = (int)((rowNum+colNum)/2);
-				}
+				//currentAmp = (int)(256*Math.random());
+				
+				rowN = rowNum; colN = colNum;
+				scaleFactor = (rowN + colN)/(height + width);
+				currentAmp = (int)(scaleFactor*256);
+				
+				imageData[0][rowNum][colNum] = currentAmp;
+				imageData[1][rowNum][colNum] = currentAmp;
+				imageData[2][rowNum][colNum] = currentAmp;
 			}
 		}
 		
-
+		
+		System.out.println("Image Data computed. Now writing image");
 		//this writes the image using the image data
 		ZachImageWriter.writeImageUsingImageSize(dummyFileName, renderedImageFileName, imageData);
 		
