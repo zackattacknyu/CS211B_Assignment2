2b4e9fbfe3aafda659b9967641ca3ce1c7b19909
zackattacknyu <zackattacknyu@gmail.com>
2014-02-16 20:59:52 -0800

    motion blur pictures generated

--- toytracer_project/ToyTracerProj/ToyTracerProj/basic_rasterizer.cpp
+++ toytracer_project/ToyTracerProj/ToyTracerProj/basic_rasterizer.cpp
@@ -23,7 +23,7 @@
 *2000 is a good number once you can wait a while. It should be lowered to 20 though
 *	when you are testing the code. 
 */
-static const int numRaysAntiAliasing = 2;
+static const int numRaysAntiAliasing = 10;
 
 struct basic_rasterizer : public Rasterizer {
     basic_rasterizer() {}
