9adceb5e9efbc6b409c41b6344d2315d0af1dd16
zackattacknyu <zackattacknyu@gmail.com>
2014-02-26 23:06:07 -0800

    modified matlab tests

--- matlab_tests/refractionFormulaTest.m
+++ matlab_tests/refractionFormulaTest.m
@@ -26,8 +26,49 @@ The formula will be as follows
 
 n_eye = 1.0;
 n_ref = 1.0;
-E = [-1 1 0];
-N = [0 1 0];
+E1 = [-1 1 0];
+E2 = [2 1 0];
+E3 = [-3 1 0];
+E4 = [5 1 0];
+E5 = [-6 1 0];
+E6 = [3 2 0];
+N = [1 8 0];
+O = [0 0 0];
 
-R = refractionDirection(n_eye,n_ref,E,N)
+N=N/norm(N);
+E1 = E1/norm(E1);
+E2 = E2/norm(E2);
+E3 = E1/norm(E3);
+E4 = E1/norm(E4);
+E5 = E1/norm(E5);
+E6 = E1/norm(E6);
 
+R1 = refractionDirection(n_eye,n_ref,E1,N);
+R2 = refractionDirection(n_eye,n_ref,E2,N);
+R3 = refractionDirection(n_eye,n_ref,E3,N);
+R4 = refractionDirection(n_eye,n_ref,E4,N);
+R5 = refractionDirection(n_eye,n_ref,E5,N);
+R6 = refractionDirection(n_eye,n_ref,E6,N);
+
+normalx = [N(1) 0 -N(1)];
+normaly = [N(2) 0 -N(2)];
+
+set1x = [E1(1) 0 R1(1)];
+set1y = [E1(2) 0 R1(2)];
+
+set2x = [E2(1) 0 R2(1)];
+set2y = [E2(2) 0 R2(2)];
+
+set3x = [E3(1) 0 R3(1)];
+set3y = [E3(2) 0 R3(2)];
+
+set4x = [E4(1) 0 R4(1)];
+set4y = [E4(2) 0 R4(2)];
+
+set5x = [E5(1) 0 R5(1)];
+set5y = [E5(2) 0 R5(2)];
+
+set6x = [E6(1) 0 R6(1)];
+set6y = [E6(2) 0 R6(2)];
+
+plot(set1x,set1y,set2x,set2y,set3x,set3y,set4x,set4y,set5x,set5y,set6x,set6y,normalx,normaly)
